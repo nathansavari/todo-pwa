80d00082a15986614e0e8afc3564f6efdae3728e
Nathan <nathan.savari.contact@gmail.com>
2024-02-18 14:15:56 +0100

    try cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,15 +20,20 @@ self.addEventListener("install", (event) => {
 });
 
 // Cache and return requests
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Cache hit - return response
-      if (response) {
-        return response;
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
       }
-      return fetch(event.request);
-    })
+      const response = await fetch(e.request);
+      const cache = await caches.open(CACHE_NAME);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
   );
 });
 
